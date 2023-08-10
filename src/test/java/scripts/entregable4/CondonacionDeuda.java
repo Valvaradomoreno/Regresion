9ae6ee7944208ae5982f2e536286adb52f953894
claynesripley <claynes@itdata.pe>
2023-08-10 10:31:50 -0500

    update entregable 4

--- src/test/java/scripts/entregable4/CondonacionDeuda.java
+++ src/test/java/scripts/entregable4/CondonacionDeuda.java
@@ -200,9 +200,9 @@ public class CondonacionDeuda {
 
 
 				wait.until(ExpectedConditions.elementToBeClickable(By.id("fieldName:NEW.BAL.AMT:2:1")));
-				//driver.findElement(By.id("fieldName:NEW.BAL.AMT:2:1")).sendKeys(monto.get(i));
-				driver.findElement(By.id("fieldName:NEW.BAL.AMT:1:1")).sendKeys(monto.get(i));
-				driver.findElement(By.id("fieldName:ADJ.BAL.AMT:2:1")).sendKeys(monto.get(i));
+				driver.findElement(By.id("fieldName:NEW.BAL.AMT:2:1")).sendKeys(monto.get(i));
+				//driver.findElement(By.id("fieldName:NEW.BAL.AMT:1:1")).sendKeys(monto.get(i));
+				//driver.findElement(By.id("fieldName:ADJ.BAL.AMT:2:1")).sendKeys(monto.get(i));
 
 				driver.findElement(By.xpath("//img[@alt='Validate a deal']")).click();
 
